482d6ff6326ab85a0eaac76a2ec75f6f65a04d77
61160177 <61160177@go.buu.ac.th>
2021-02-23 15:22:27 +0700

    login register

--- app/User.php
+++ app/User.php
@@ -10,13 +10,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $primaryKey = 'user_id';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname','lastname', 'email', 'password',
     ];
 
     /**
