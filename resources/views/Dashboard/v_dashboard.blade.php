2469b8c60a48907850fb6d2b35337268106c18a0
Thanach Jintakanont <61160060@go.buu.ac.th>
2021-02-24 15:16:22 +0700

    update: menu

--- resources/views/Dashboard/v_dashboard.blade.php
+++ resources/views/Dashboard/v_dashboard.blade.php
@@ -1,1 +1,19 @@
-test
+@extends('menu')
+@section('title','dashboard')
+
+@section('content')
+<!-- Page wrapper -->
+<!DOCTYPE html>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">รายงานสรุปเชิงสถิติ</li>
+    </ol>
+  </nav>
+
+
+
+
+    @section('script')
+
+    @stop
+@stop
