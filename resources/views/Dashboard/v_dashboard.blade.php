a7e633f668b387d991b81fb2f5c961284c704f98
Apichaya <61160080@go.buu.ac.th>
2021-02-24 15:43:03 +0700

    chart

--- resources/views/Dashboard/v_dashboard.blade.php
+++ resources/views/Dashboard/v_dashboard.blade.php
@@ -10,10 +10,34 @@
     </ol>
   </nav>
 
+  
+  
+    <!-- Chart's container -->
+    <div id="chart" style="height: 300px;"></div>
+    <!-- Charting library -->
+    <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+    <!-- Chartisan -->
+    <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+    <!-- Your application script -->
+    
+  
+
+
 
 
 
     @section('script')
 
+    <script>
+      const chart = new Chartisan({
+        el: '#chart',
+        url: "@chart('sample_chart')",
+        hooks: new ChartisanHooks()
+          .colors()
+          .datasets(['line']),
+      });
+    </script>
+
     @stop
 @stop
+
