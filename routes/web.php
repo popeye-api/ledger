6bd6c3a1c66b3b26f13f763da466056f8a7def31
Thanisorn Amnuay <>
2021-02-24 15:08:36 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('v_dashboard','Dashboard_controller@show_dashboard');
+
+Route::resource('report','Report_controller');
