ce4def4fa6408112a0474e195fc8e6688b017243
Thanach Jintakanont <61160060@go.buu.ac.th>
2021-02-27 13:52:19 +0700

    create : v_dashboard_year update : routes

---
+++ resources/views/Dashboard/v_dashboard_year.blade.php
@@ -0,0 +1,27 @@
+@extends('menu')
+@section('title','dashboard')
+
+@section('content')
+<!-- Page wrapper -->
+<!DOCTYPE html>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item" aria-current="page"><a href="{{ url('v_dashboard') }}">Dashbaord</a></li>
+            <li class="breadcrumb-item active" aria-current="page">2018</li>
+        </ol>
+    </nav>
+
+
+  
+
+
+
+
+    @section('script')
+
+    <script>
+
+    </script>
+
+    @stop
+@stop
