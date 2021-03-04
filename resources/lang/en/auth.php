30eb53b70678d0b32100b92b453012d81e47f628
61160177 <70052049+61160177@users.noreply.github.com>
2021-03-05 00:23:06 +0700

    Update auth.php
    forgot

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
