a241081d33760e11d09a463e52d5b8136f7c6875
61160177 <70052049+61160177@users.noreply.github.com>
2021-03-04 21:13:17 +0700

    Update login.blade.php
    layout login ทำโครงไว้แล้ว จัดแค่ text input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,81 @@
-{{-- @extends('layouts.app') --}}
-@extends('menu')
+@extends('layouts.template')
+{{-- @extends('menu') --}}
 
 @section('content')
+@php
+    $urlImg = asset('/storage/bg.jpg');    
+@endphp
+
+
 <div class="container">
+    <div class="card card-size">
+    <div class="row">
+        
+        <div class="col-md-4 ">
+            <img src="{{$urlImg}}"class="img-bg" >
+        </div>
+
+        <div class="col-md-8 ">
+            <div class="card-body">
+  จัด layout ด้วยยยย
+  <form method="POST" action="{{ route('login') }}">
+    @csrf
+
+    <div class="form-group row">
+        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+        <div class="col-md-6">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+        <div class="col-md-6">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+    
+
+    <div class="form-group row mb-0">
+        <div class="col-md-8 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+                {{ __('Login') }}
+            </button>
+
+            @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            @endif
+        </div>
+    </div>
+</form>
+            </div>
+          </div>
+    </div>
+    </div>
+
+  </div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card img-bg">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -70,5 +140,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
