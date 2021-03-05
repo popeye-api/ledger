8d16c7ee1ae97e3b1ee36ad3bd98ddbf1ad8617f
61160177 <70052049+61160177@users.noreply.github.com>
2021-03-05 09:04:07 +0700

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -35,7 +35,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="background-color:#FF7A33;">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
