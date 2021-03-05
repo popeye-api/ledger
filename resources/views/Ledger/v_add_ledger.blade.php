55c0520d2a64167d44bc16e1f4b1c923ff801d90
DESKTOP-89PM28N\Chogi <61160277@go.buu.ac.th>
2021-03-05 07:09:42 +0700

    Ledger add update delete button

--- resources/views/Ledger/v_add_ledger.blade.php
+++ resources/views/Ledger/v_add_ledger.blade.php
@@ -45,7 +45,14 @@
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Balance  </label>
                 <label class="col-md-1 "><input class="form-control" type="text" placeholder="+200">  </label>
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Description  </label>
-                <label class="col-md-2 "><input class="form-control" type="text" placeholder="-">  </label>
+                <label class="col-md-1 "><input class="form-control" type="text" placeholder="-" style = "width: 170px; overflow: hidden;">  </label>
+                <label class="col-md-1 " style = "text-align:right;">
+                <button  class="btn btn-danger width: 100px; overflow: hidden;"  type="text" placeholder="-">  
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash menu-icon " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    </svg>
+                </label>
         </li>
         <li class="list-group-item">
                 <label class="col-md-2 " style = "padding : 6px; text-align : center;"> Product Name  </label>
@@ -62,7 +69,14 @@
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Balance  </label>
                 <label class="col-md-1 "><input class="form-control" type="text" placeholder="+500">  </label>
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Description  </label>
-                <label class="col-md-2 "><input class="form-control" type="text" placeholder="-">  </label>
+                <label class="col-md-1 "><input class="form-control" type="text" placeholder="-" style = "width: 170px; overflow: hidden;">  </label>
+                <label class="col-md-1 " style = "text-align:right;">
+                <button  class="btn btn-danger width: 100px; overflow: hidden;"  type="text" placeholder="-">  
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash menu-icon " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    </svg>
+                </label>
         </li>
         <li class="list-group-item">
                 <label class="col-md-2 " style = "padding : 6px; text-align : center;"> Product Name  </label>
@@ -79,7 +93,14 @@
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Balance  </label>
                 <label class="col-md-1 "><input class="form-control" type="text" placeholder="-500">  </label>
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Description  </label>
-                <label class="col-md-2 "><input class="form-control" type="text" placeholder="-">  </label>
+                <label class="col-md-1 "><input class="form-control" type="text" placeholder="-" style = "width: 170px; overflow: hidden;">  </label>
+                <label class="col-md-1 " style = "text-align:right;">
+                <button  class="btn btn-danger width: 100px; overflow: hidden;"  type="text" placeholder="-">  
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash menu-icon " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    </svg>
+                </label>
         </li>
         <li class="list-group-item">
                 <label class="col-md-2 " style = "padding : 6px; text-align : center;"> Product Name  </label>
@@ -96,7 +117,14 @@
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Balance  </label>
                 <label class="col-md-1 "><input class="form-control" type="text" placeholder="-">  </label>
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Description  </label>
-                <label class="col-md-2 "><input class="form-control" type="text" placeholder="-">  </label>
+                <label class="col-md-1 "><input class="form-control" type="text" placeholder="-" style = "width: 170px; overflow: hidden;">  </label>
+                <label class="col-md-1 " style = "text-align:right;">
+                <button  class="btn btn-danger width: 100px; overflow: hidden;"  type="text" placeholder="-">  
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash menu-icon " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    </svg>
+                </label>
         </li>
         <li class="list-group-item">
                 <label class="col-md-2 " style = "padding : 6px; text-align : center;"> Product Name  </label>
@@ -113,7 +141,14 @@
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Balance  </label>
                 <label class="col-md-1 "><input class="form-control" type="text" placeholder="-">  </label>
                 <label class="col-md-1 " style = "padding : 6px; text-align : center;">Description  </label>
-                <label class="col-md-2 "><input class="form-control" type="text" placeholder="-">  </label>
+                <label class="col-md-1 "><input class="form-control" type="text" placeholder="-" style = "width: 170px; overflow: hidden;">  </label>
+                <label class="col-md-1 " style = "text-align:right;">
+                <button  class="btn btn-danger width: 100px; overflow: hidden;"  type="text" placeholder="-">  
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash menu-icon " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    </svg>
+                </label>
         </li>
     </div>
 
