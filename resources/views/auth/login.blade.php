5b4e8725201c1a272405e3ff556cf8fdcccc1042
61160177 <70052049+61160177@users.noreply.github.com>
2021-03-05 09:35:28 +0700

    Update login.blade.php
    login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,8 +56,8 @@
     
 
     <div class="form-group row mb-0">
-        <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary" style="background-color:#FF7A33;">
+        <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary btn-block" style="background-color:#FF7A33;">
                 {{ __('Login') }}
             </button>
             
