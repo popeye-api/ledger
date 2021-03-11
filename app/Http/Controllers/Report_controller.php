e25474949977ca3c6969d6b4038efaf21473418e
Thanisorn Amnuay <>
2021-03-11 23:49:52 +0700

    report

--- app/Http/Controllers/Report_controller.php
+++ app/Http/Controllers/Report_controller.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Exports\LedgerExport;
+use App\Statement_log;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
 class Report_controller extends Controller
 {
 
     public function index()
     {
-        return view('Report.v_report');
+        $user = Auth::User()->user_id;
+        $data = new Statement_log();
+        $rs_log_year = $data->get_log_year($user);
+        return view('Report.v_report')->with('result',$rs_log_year);
     }
 
 
--- resources/views/Report/v_report.blade.php
+++ resources/views/Report/v_report.blade.php
@@ -50,7 +50,7 @@
             <?php 
               $balance = 0;
               foreach($result as $row){
-                if(date('m',strtotime($row->created_at)) == 01){
+                if(date('m',strtotime($row->created_at)) == '01'){
                   if($row->log_type_id == 1){
                     $balance += $row->balance;
                   }
@@ -67,7 +67,7 @@
             <?php 
               $balance_feb = 0;
               foreach($result as $row){
-                if(date('m',strtotime($row->created_at)) == 02){
+                if(date('m',strtotime($row->created_at)) == '02'){
                   if($row->log_type_id == 1){
                     $balance_feb += $row->balance;
                   }
@@ -84,7 +84,7 @@
             <?php 
               $balance_mar = 0;
               foreach($result as $row){
-                if(date('m',strtotime($row->created_at)) == 03){
+                if(date('m',strtotime($row->created_at)) == '03'){
                   if($row->log_type_id == 1){
                     $balance_mar += $row->balance;
                   }
@@ -98,30 +98,156 @@
             </tr>
             <tr>
             <td>April</td>
+            <?php 
+              $balance_april = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '04'){
+                  if($row->log_type_id == 1){
+                    $balance_april += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_april -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_april ?></td>
             </tr>
             <tr>
             <td>May</td>
+            <?php 
+              $balance_may = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '05'){
+                  if($row->log_type_id == 1){
+                    $balance_may += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_may -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_may ?></td>
             </tr>
             <tr>
             <td>June</td>
+            <?php 
+              $balance_june = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '06'){
+                  if($row->log_type_id == 1){
+                    $balance_june += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_june -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_june ?></td>
             </tr>
             <tr>
             <td>July</td>
+            <?php 
+              $balance_july = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '07'){
+                  if($row->log_type_id == 1){
+                    $balance_july += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_july -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_july ?></td>
             </tr>
             <tr>
             <td>August</td>
+            <?php 
+              $balance_aug = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '08'){
+                  if($row->log_type_id == 1){
+                    $balance_aug += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_aug -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_aug ?></td>
             </tr>
             <tr>
             <td>September</td>
+            <?php 
+              $balance_sep = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '09'){
+                  if($row->log_type_id == 1){
+                    $balance_sep += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_sep -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_sep ?></td>
             </tr>
             <tr>
             <td>October </td>
+            <?php 
+              $balance_oct = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '10'){
+                  if($row->log_type_id == 1){
+                    $balance_oct += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_oct -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_oct ?></td>
             </tr>
             <tr>
             <td>November </td>
+            <?php 
+              $balance_nov = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '11'){
+                  if($row->log_type_id == 1){
+                    $balance_nov += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_nov -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_nov ?></td>
             </tr>
             <tr>
             <td>December </td>
+            <?php 
+              $balance_dec = 0;
+              foreach($result as $row){
+                if(date('m',strtotime($row->created_at)) == '12'){
+                  if($row->log_type_id == 1){
+                    $balance_dec += $row->balance;
+                  }
+                  if($row->log_type_id == 2){
+                    $balance_dec -= $row->balance;
+                  }
+                }
+              }
+            ?>
+            <td><?php echo $balance_dec ?></td>
             </tr>
             <tr>
             <td colspan="2" style="text-align: center;">Total </td>
